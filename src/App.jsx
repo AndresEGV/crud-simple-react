ceeb6313f1b3a52a8b477ac2573e83ecc45032da
Andres <agomez.vg@gmail.com>
2021-09-24 16:02:55 -0300

    Formulario ok y listar tareas funcionando

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+function App() {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    console.log(tarea);
+    setTareas([...tareas, { id: nanoid(10), nombreTarea: tarea }]);
+    setTarea("");
+  };
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Crud simple</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">
+            {tareas.map((item) => (
+              <li className="list-group-item" key={item.id}>
+                <span className="lead">{item.nombreTarea}</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">
+                  Eliminar
+                </button>
+                <button className="btn btn-warning btn-sm float-right">
+                  Editar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={agregarTarea}>
+            <input
+              className="form-control mb-2"
+              placeholder="Ingrese tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button className="btn btn-dark btn-block" type="submit">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
