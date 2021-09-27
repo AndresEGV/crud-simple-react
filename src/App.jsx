a020ee5172186bfb22349d85af8247fd8a044c1e
Andres <agomez.vg@gmail.com>
2021-09-27 12:46:27 -0300

    Crud finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,49 @@ import { nanoid } from "nanoid";
 function App() {
   const [tarea, setTarea] = useState("");
   const [tareas, setTareas] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null);
+
   const agregarTarea = (e) => {
     e.preventDefault();
-    console.log(tarea);
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      setError("Escriba algo por favor");
+      return;
+    }
+
     setTareas([...tareas, { id: nanoid(10), nombreTarea: tarea }]);
     setTarea("");
+    setError(null);
+  };
+  const eliminarTarea = (id) => {
+    const arrayFiltrado = tareas.filter((item) => item.id !== id);
+    setTareas(arrayFiltrado);
+  };
+
+  const editar = (item) => {
+    setModoEdicion(true);
+    setTarea(item.nombreTarea);
+    setId(item.id);
+    console.log(item);
+  };
+
+  const editarTarea = (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      setError("Escriba algo por favor");
+      return;
+    }
+    const arrayEditado = tareas.map((item) =>
+      item.id === id ? { id, nombreTarea: tarea } : item
+    );
+    setTareas(arrayEditado);
+    setModoEdicion(false);
+    setTarea("");
+    setId("");
+    setError(null);
   };
   return (
     <div className="container mt-5">
@@ -17,31 +55,50 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            {tareas.map((item) => (
-              <li className="list-group-item" key={item.id}>
-                <span className="lead">{item.nombreTarea}</span>
-                <button className="btn btn-danger btn-sm float-right mx-2">
-                  Eliminar
-                </button>
-                <button className="btn btn-warning btn-sm float-right">
-                  Editar
-                </button>
-              </li>
-            ))}
+            {tareas.length === 0 ? (
+              <li className="list-group-item">No hay tareas</li>
+            ) : (
+              tareas.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">{item.nombreTarea}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => eliminarTarea(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm float-right"
+                    onClick={() => editar(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))
+            )}
           </ul>
         </div>
         <div className="col-4">
-          <h4 className="text-center">Formulario</h4>
-          <form onSubmit={agregarTarea}>
+          <h4 className="text-center">
+            {modoEdicion ? "Editar Tarea" : "Agregar Tarea"}
+          </h4>
+          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+            {error ? <span className="text-danger">{error}</span> : null}
             <input
               className="form-control mb-2"
               placeholder="Ingrese tarea"
               onChange={(e) => setTarea(e.target.value)}
               value={tarea}
             />
-            <button className="btn btn-dark btn-block" type="submit">
-              Agregar
-            </button>
+            {modoEdicion ? (
+              <button className="btn btn-warning btn-block" type="submit">
+                Editar
+              </button>
+            ) : (
+              <button className="btn btn-dark btn-block" type="submit">
+                Agregar
+              </button>
+            )}
           </form>
         </div>
       </div>
